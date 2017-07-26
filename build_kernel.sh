4620533b533ff11c38877b597c561eda209fdd2f
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-07-26 16:44:17 +0200

    build_kernel: log make output and add git exception for .log files
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,4 +4,4 @@ export ARCH=arm64
 export CROSS_COMPILE=../gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 make exynos8890-hero2lte_defconfig
-make -j4
+make -j4 2>&1 | tee -a ./build_kernel.log
