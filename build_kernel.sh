11fa04f2bc47248cc2e49a67f19eb1ac6c27f4aa
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-07-28 11:35:21 +0200

    build_kernel: add generic leftovers cleaning function
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -48,6 +48,35 @@ FUNC_CLEAN_ENVIRONMENT()
 	exit 1
 }
 
+FUNC_CUSTOM_CLEAN()
+{
+	tput reset
+
+	echo ""
+	echo "=================================================================="
+	echo "START : FUNC_CUSTOM_CLEAN"
+	echo "=================================================================="
+	echo ""
+
+	echo "Deleting log files..."
+	rm -f cfp_log.txt
+	rm -f build_kernel.log
+	rm -f build/herolte/.version
+	rm -f build/hero2lte/.version
+	echo ""
+	echo "Deleting patch related leftovers..."
+	rm -rf *.patch
+	rm -rf *.diff
+
+	echo ""
+	echo "=================================================================="
+	echo "END   : FUNC_CUSTOM_CLEAN"
+	echo "=================================================================="
+	echo ""
+
+	exit 1
+}
+
 FUNC_UNKNOWN_INPUT()
 {
 	tput reset
@@ -66,6 +95,7 @@ FUNC_UNKNOWN_INPUT()
 	echo "3 - to build for hero2lteeur"
 	echo "4 - to build for hero2ltekor"
 	echo ""
+	echo "9 - to run the custom cleaning routine"
 	echo "0 - to clean the build environment"
 	echo ""
 	echo "=================================================================="
@@ -78,6 +108,8 @@ FUNC_UNKNOWN_INPUT()
 
 if [ $1 == 0 ]; then
 	FUNC_CLEAN_ENVIRONMENT
+elif [ $1 == 9 ]; then
+	FUNC_CUSTOM_CLEAN
 elif [ $1 == 1 ] || [ $1 == 2 ]; then
 	export MODEL=herolte
 elif [ $1 == 3 ] || [ $1 == 4 ]; then
