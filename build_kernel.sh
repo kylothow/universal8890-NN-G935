75a156135ebd7d7a89492743d161c00c2a43e0ce
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-07-31 01:11:42 +0200

    build_kernel: use custom kernel name and version
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+KERNEL_NAME="Primal_Kernel"
+KERNEL_VERSION="2.0.0"
+KERNEL_BETA="true"
+
+if [ $KERNEL_BETA == true ]; then
+	KERNEL_VERSION+=β
+fi
+export LOCALVERSION=-${KERNEL_NAME}-v${KERNEL_VERSION}
+
 export ARCH=arm64
 export CROSS_COMPILE=../gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
